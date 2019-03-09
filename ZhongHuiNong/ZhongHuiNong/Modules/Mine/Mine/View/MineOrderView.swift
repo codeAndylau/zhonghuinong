fc293486b2d29ad021b933d4885ff92fdebd0b06
Andylau_code <447696672@qq.com>
2019-03-09 09:59:56 +0800

    MineOrder-UI 修改

--- ZhongHuiNong/ZhongHuiNong/Modules/Mine/Mine/View/MineOrderView.swift
+++ ZhongHuiNong/ZhongHuiNong/Modules/Mine/Mine/View/MineOrderView.swift
@@ -21,24 +21,56 @@ class MineOrderView: View {
         btn.setTitleColor(UIColor.hexColor(0x333333), for: .normal)
         btn.titleLabel?.font = UIFont.systemFont(ofSize: 12)
         btn.adjustsImageWhenHighlighted = false
+        btn.lc_setLocalImage(with: "mine_fukuan")
+    }
+    
+    let fukuanLab = Label().then { (lab) in
+        lab.text = "待付款"
+        lab.textColor = UIColor.hexColor(0x333333)
+        lab.textAlignment = .left
+        lab.font = UIFont.systemFont(ofSize: 12)
     }
     
     let peisongBtn = Button().then { (btn) in
         btn.setTitleColor(UIColor.hexColor(0x333333), for: .normal)
         btn.titleLabel?.font = UIFont.systemFont(ofSize: 12)
         btn.adjustsImageWhenHighlighted = false
+        btn.lc_setLocalImage(with: "mine_peisong")
+    }
+    
+    let peisongLab = Label().then { (lab) in
+        lab.text = "待配送"
+        lab.textColor = UIColor.hexColor(0x333333)
+        lab.textAlignment = .left
+        lab.font = UIFont.systemFont(ofSize: 12)
     }
     
     let shouhuoBtn = Button().then { (btn) in
         btn.setTitleColor(UIColor.hexColor(0x333333), for: .normal)
         btn.titleLabel?.font = UIFont.systemFont(ofSize: 12)
         btn.adjustsImageWhenHighlighted = false
+        btn.lc_setLocalImage(with: "mine_shouhuo")
+    }
+    
+    let shouhuoLab = Label().then { (lab) in
+        lab.text = "待收货"
+        lab.textColor = UIColor.hexColor(0x333333)
+        lab.textAlignment = .left
+        lab.font = UIFont.systemFont(ofSize: 12)
     }
     
     let orderBtn = Button().then { (btn) in
         btn.setTitleColor(UIColor.hexColor(0x333333), for: .normal)
         btn.titleLabel?.font = UIFont.systemFont(ofSize: 12)
         btn.adjustsImageWhenHighlighted = false
+        btn.lc_setLocalImage(with: "mine_order")
+    }
+    
+    let allOrderLab = Label().then { (lab) in
+        lab.text = "全部订单"
+        lab.textColor = UIColor.hexColor(0x333333)
+        lab.textAlignment = .left
+        lab.font = UIFont.systemFont(ofSize: 12)
     }
     
     override func makeUI() {
@@ -48,10 +80,16 @@ class MineOrderView: View {
         addSubview(peisongBtn)
         addSubview(shouhuoBtn)
         addSubview(orderBtn)
+        
+        fukuanBtn.addSubview(fukuanLab)
+        peisongBtn.addSubview(peisongLab)
+        shouhuoBtn.addSubview(shouhuoLab)
+        orderBtn.addSubview(allOrderLab)
         activateConstraints()
     }
     
     func activateConstraints() {
+        
         orderLab.snp.makeConstraints { (make) in
             make.top.equalTo(self).offset(10)
             make.left.equalTo(self).offset(15)
@@ -65,10 +103,20 @@ class MineOrderView: View {
             make.height.equalTo(80)
         }
         
+        fukuanLab.snp.makeConstraints { (make) in
+            make.bottom.equalTo(fukuanBtn.snp.bottom)
+            make.centerX.equalTo(fukuanBtn)
+        }
+        
         peisongBtn.snp.makeConstraints { (make) in
             make.left.equalTo(fukuanBtn.snp.right)
             make.centerY.equalTo(fukuanBtn)
             make.width.height.equalTo(fukuanBtn)
+        }
+        
+        peisongLab.snp.makeConstraints { (make) in
+            make.bottom.equalTo(peisongBtn.snp.bottom)
+            make.centerX.equalTo(peisongBtn)
         }
         
         shouhuoBtn.snp.makeConstraints { (make) in
@@ -77,20 +125,30 @@ class MineOrderView: View {
             make.width.height.equalTo(peisongBtn)
         }
         
+        shouhuoLab.snp.makeConstraints { (make) in
+            make.bottom.equalTo(shouhuoBtn.snp.bottom)
+            make.centerX.equalTo(shouhuoBtn)
+        }
+        
         orderBtn.snp.makeConstraints { (make) in
             make.left.equalTo(shouhuoBtn.snp.right)
             make.centerY.equalTo(shouhuoBtn)
             make.width.height.equalTo(shouhuoBtn)
+        }
+        
+        allOrderLab.snp.makeConstraints { (make) in
+            make.bottom.equalTo(orderBtn.snp.bottom)
+            make.centerX.equalTo(orderBtn)
         }
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        fukuanBtn.set(image: UIImage(named: "mine_fukuan"), title: "待付款", titlePosition: .bottom, additionalSpacing: 0, state: .normal)
-        peisongBtn.set(image: UIImage(named: "mine_peisong"), title: "待配送", titlePosition: .bottom, additionalSpacing: 0, state: .normal)
-        shouhuoBtn.set(image: UIImage(named: "mine_shouhuo"), title: "待收货", titlePosition: .bottom, additionalSpacing: 0, state: .normal)
-        orderBtn.set(image: UIImage(named: "mine_order"), title: "全部订单", titlePosition: .bottom, additionalSpacing: 0, state: .normal)
+//        fukuanBtn.set(image: UIImage(named: "mine_fukuan"), title: "待付款", titlePosition: .bottom, additionalSpacing: 0, state: .normal)
+//        peisongBtn.set(image: UIImage(named: "mine_peisong"), title: "待配送", titlePosition: .bottom, additionalSpacing: 0, state: .normal)
+//        shouhuoBtn.set(image: UIImage(named: "mine_shouhuo"), title: "待收货", titlePosition: .bottom, additionalSpacing: 0, state: .normal)
+//        orderBtn.set(image: UIImage(named: "mine_order"), title: "全部订单", titlePosition: .bottom, additionalSpacing: 0, state: .normal)
     }
 
     
